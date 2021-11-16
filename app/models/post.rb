2f391ba71cd0636c84955543057f611dbd0ec686
MShajieShah <shajieshah@gmail.com>
2021-11-16 13:51:00 +0500

    CSS added

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,7 @@
 class Post < ApplicationRecord
+  validates :title, presence: true,
+                    length: { minimum: 5 }
   belongs_to :user
   has_many :sub_posts, dependent: :destroy
-  accepts_nested_attributes_for :sub_posts, reject_if: :all_blank,allow_destroy: true
+  accepts_nested_attributes_for :sub_posts, reject_if: :all_blank, allow_destroy: true
 end
