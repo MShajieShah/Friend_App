de266deb9a19b9b60186e71b14433a4296035353
MShajieShah <shajieshah@gmail.com>
2021-11-16 19:07:35 +0500

    Add Public or Private posts feature

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_10_124333) do
+ActiveRecord::Schema.define(version: 2021_11_16_102224) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -33,6 +33,7 @@ ActiveRecord::Schema.define(version: 2021_11_10_124333) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.integer "user_id"
+    t.boolean "published"
   end
 
   create_table "sub_posts", force: :cascade do |t|
